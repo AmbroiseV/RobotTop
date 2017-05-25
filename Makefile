46db6b7c2e2d79e4452345829250b989e27702d1
superpingu <arnaudbonetti@me.com>
2017-05-25 02:02:07 +0200

    tests

--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
 TARGET  = robottop
-SRCS    = main.c
+SRCS    = $(TARGET).c
 CC      = gcc
-CFLAGS  = -O2 -std=gnu99 -Wall -Werror
+CFLAGS  = -O2 -std=gnu99 -Wall 
 LDFLAGS = -lrobotdriver -lm
 
 all: $(TARGET)
 
+test: tests
+	./$<
+
 clean:
-	rm -f $(TARGET)
+	rm -f $(TARGET) tests
--- robottop.c
+++ robottop.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 
 #include <robotdriver/movecontroller.h>
+#include <robotdriver/motordriver.h>
+#include <robotdriver/timing.h>
 //#include "capteurs.h"
 
 #define TAILLE_MAX 32
@@ -11,6 +13,11 @@
 #define STARTRADIUS 30
 
 int readAndCall(FILE * file, char c);
+
+// 0: translation, 1: rotation
+static int mov = 0;
+// 0: forward, 1: backward
+static int dir = 0;
 
 int main()
 {
@@ -28,12 +35,14 @@ int main()
     //ouvre le fichier et la tache sera stocké dans action
     char c;
     FILE* file = NULL;
-    file = fopen("commande.txt", "r");
-
-    while((c = fgetc(file)) != EOF)
+    file = fopen("command_tests.txt", "r");
+    c = fgetc(file);
+    while(c != EOF && c != '\0')
+    {
         if(readAndCall(file, c))
 	    return 0;
-
+        c = fgetc(file);
+    }
     fclose(file);
     return 0;
 }
@@ -41,32 +50,61 @@ int main()
 int readAndCall(FILE * file, char c)
 {
     if((c >= '0') && (c <= '9')) {
-      char Xcoord[TAILLE_COORDONNEES];
-      char Ycoord[TAILLE_COORDONNEES];
-      Xcoord[0] = c;
-      int i = 1;
-      while((c = fgetc(file)) != ' ')
-        Xcoord[i++] = c;
-      i = 0;
-      while((c = fgetc(file)) != '\n')
-        Ycoord[i++] = c;
-      int x = atoi(Xcoord);
-      int y = atoi(Ycoord);
-      printf("[FILE] go to [%d,%d]\n", x, y);
-      point_t dest = {x, y};
-      goForward(&dest);
-      return 0;
+	if(!mov){
+    	    char Xcoord[TAILLE_COORDONNEES];
+    	    char Ycoord[TAILLE_COORDONNEES];
+    	    Xcoord[0] = c;
+    	    int i = 1;
+    	    while((c = fgetc(file)) != ' ')
+    	        Xcoord[i++] = c;
+    	    i = 0;
+    	    while((c = fgetc(file)) != '\n')
+    	        Ycoord[i++] = c;
+    	    int x = atoi(Xcoord);
+    	    int y = atoi(Ycoord);
+    	    printf("[FILE] go to [%d,%d]\n", x, y);
+    	    point_t dest = {x, y};
+            goForward(&dest);
+            return 0;
+	}
+        else {
+    	    char rot_str[TAILLE_COORDONNEES];
+    	    rot_str[0] = c;
+    	    int i = 1;
+    	    while((c = fgetc(file)) != '\n')
+    	        rot_str[i++] = c;
+    	    int rot = atoi(rot_str);
+    	    printf("[FILE] go to [%ddeg]\n", rot / 10);
+            rotate(rot);
+            return 0;
+	}
     }
-    fgetc(file);
-    if(c == 'A'){
+    while(fgetc(file) != '\n');
+    if(c == 'P'){
+        printf("[FILE] It's an P \n");
+    }
+    else if(c == 'S'){
+        printf("[FILE] It's a S \n");
+    }
+    else if(c == 'L'){
+        printf("[FILE] It's a L \n");
+    }
+    else if(c == 'A'){
         printf("[FILE] It's an A \n");
+	mov = 0;
+	dir = 0;
     }
-    else if(c == 'B'){
-        printf("[FILE] It's a B \n");
+    else if(c == 'R'){
+        printf("[FILE] It's a R \n");
+	mov = 0;
+	dir = 1;
     }
-    else if(c == 'C'){
-        printf("[FILE] It's a C \n");
+    else if(c == 'D'){
+        printf("[FILE] It's a D \n");
+	mov = 1;
     }
+    else if(c == '#'){}
+    else if(c == '\n'){}
     else {
 	if(c != 255)
             printf("[FILE] PARSING ERROR: %d\n", c);
